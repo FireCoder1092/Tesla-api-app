fcc2f912cb96298c09a88ec585d130a429aabcd1
FireCoder1092 <smaranpreethesh@gmail.com>
2025-05-30 16:28:15 +1200

    Added title & description

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests as r
 
 api_key = "95ac2c9588624285b77362492f8f7909"
-url = ("https://newsapi.org/v2/everything?q=tesla&"
-       "from=2025-04-18&sortBy=publishedAt&apiKey="
-       "95ac2c9588624285b77362492f8f7909")
+url = ("https://newsapi.org/v2/everything?q=tesla&from="
+       "2025-04-30&sortBy=publishedAt&apiKey=95ac2c9588"
+       "624285b77362492f8f7909")
 
 re = r.get(url)
-content = re.text
-print(content)
+content = re.json()
+for article in content["articles"]:
+       print(article["title"])
+       print(article["description"])
