c173076975dfc72b11b70574be91e3aaaf7f2bec
FireCoder1092 <smaranpreethesh@gmail.com>
2025-05-31 14:50:10 +1200

    Add improvements

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import requests as r
 import os
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = os.getenv("NewsApiKey")
-url = ("https://newsapi.org/v2/everything?q=tesla&from="
-       "2025-04-30&sortBy=publishedAt&apiKey=95ac2c9588"
-       "624285b77362492f8f7909")
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "from=2025-04-30&"
+       "sortBy=publishedAt&"
+       "apiKey=95ac2c9588624285b77362492f8f7909&"
+       "language=en")
 
 re = r.get(url)
 content = re.json()
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:5]:
        if article["title"] and article["description"] is not None:
-              body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+              body = (body + "Subject: Today's News" + "\n" + article["title"] + "\n" +
+                      article["description"] + "\n" +
+                      article["url"] + 2*"\n")
 
 body = body.encode("utf-8")
 send_email(message=body)
